8b2cfed58d000f8fb131f864f43f72226ac172d3
IBN12 <IBN12@github.com>
2023-02-03 21:19:26 -0800

    Assignment 4 has been commenced. Working on the initial layout of the page.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 console.log("Testing...");
 console.log("Second Test...");
+
+// import the css file:
+import './style.css';
+
+// import the image files:
+import pageBackground from './images/Concept-Art-12.png';
+
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,19 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+
+    // module loader
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
